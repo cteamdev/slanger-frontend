327469f78e23031ca455692b6d9b38d109d4eabb
ItzNeviKat <nikitakoschelenko@gmail.com>
2021-10-24 12:58:11 +0300

    fix: Белая тема при загрузке с темной темой

--- src/bridge.ts
+++ src/bridge.ts
@@ -1,17 +1,22 @@
 import { send, subscribe, VKBridgeEvent } from '@vkontakte/vk-bridge';
 
 export const init = (): void => {
+  const root: HTMLElement = document.getElementById('root')!;
+  root.style.opacity = '0';
+
   subscribe((e: VKBridgeEvent<any>) => {
     switch (e.detail.type) {
       case 'VKWebAppUpdateConfig': {
         const scheme: string = e.detail.data.scheme || 'client_light';
         document.body.setAttribute('scheme', scheme);
+
+        root.style.opacity = '1';
+
         break;
       }
       default:
         return;
     }
   });
-
   send('VKWebAppInit');
 };
