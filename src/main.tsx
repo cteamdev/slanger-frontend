3b8c01e336330af3886b6c832bbc692c8e9757db
ItzNeviKat <nikitakoschelenko@gmail.com>
2021-10-24 11:11:41 +0300

    fix: Динамический импорт для eruda

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,10 @@ import { render } from 'react-dom';
 import { init } from './bridge';
 import { App } from './App';
 
-import './eruda';
-// import './sentry';
-
 init();
 render(<App />, document.getElementById('root'));
 
+import('./eruda');
 console.warn(`
                   @@@                          
             @@@@@@@@@@@@@@@                    
